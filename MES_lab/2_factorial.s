9c6e83439baf26b1901e80bc0fef51a404aab169
Mohammed Ali Alsakkaf (Binbasri) <87440507+Binbasri-in@users.noreply.github.com>
2023-07-30 21:02:55 +0530

    Update 2_factorial.s

--- MES_lab/2_factorial.s
+++ MES_lab/2_factorial.s
@@ -1,11 +1,11 @@
       AREA FACTORIAL,CODE,READONLY
 START
     MOV R0,#10  ; R0 = 10
-    MOV R1,#0   ; R1 = 0
+    MOV R1,R0   ; R1 = 10
 
 AGAIN
-    MUL R1,R0,R1   ; R1 = R0 * R1
     SUB R0,#1   ; R0 = R0 - 1
+    MUL R1,R0,R1   ; R1 = R0 * R1
     CMP R0,#1   ; R0 - 0 == 0?
     BNE AGAIN   ; if not equal, jump to AGAIN
 
